194763be0d74135ab7be5773fcd4e1cf58e2453e
TimothyElder <timothyelder@uchicago.edu>
2023-12-23 22:50:37 -0500

    fixed get-data.sh

--- scripts/get-data.sh
+++ scripts/get-data.sh
@@ -1,2 +1,9 @@
-wget https://data.ntsb.gov/avdata/FileDirectory/DownloadFile?fileID=C%3A%5Cavdata%5Cavall.zip
+#!/bin/bash
+
+cd ../data
+
+url="https://data.ntsb.gov/avdata/FileDirectory/DownloadFile?fileID=C%3A%5Cavdata%5Cavall.zip"
+
+# Download the file
+wget "$url"
 unzip avall.zip
